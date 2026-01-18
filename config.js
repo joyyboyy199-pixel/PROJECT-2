4249a87936736d59010dab86a1f09e09ed819b57
joyyboyy199-pixel <joyyboyy199@gmail.com>
2026-01-18 17:39:42 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    // LINK IKLAN BANNER (ATAS)
+    adKey: "fee74d02816fa33c6ddff98e6df77ccf",
+    
+    // LINK TUJUAN SETELAH 3 DETIK
+    targetUrl: "https://playfloowz.com/c?a=439&o=13&sub1=NFL_NET"
+};
